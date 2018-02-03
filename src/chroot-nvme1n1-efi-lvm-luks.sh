588ae3f256a70639a513c728a7dd1ab443f7e31f
Matthias Geisler <matthias@openwebcraft.com>
2018-02-04 00:24:25 +0100

    WIP **NOT TESTED** for efi w/ luks lvm and gummiboot

---
+++ src/chroot-nvme1n1-efi-lvm-luks.sh
@@ -0,0 +1,43 @@
+#Potential variables: timezone, lang and local
+
+passwd
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+hwclock --systohc --utc
+
+echo "en_DK.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_DK.ISO-8859-1 ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+
+echo "initramfs creation..."
+sleep 2
+file=/mnt/archbox/etc/mkinitcpio.conf
+search="^\s*HOOKS=.*$"
+replace="HOOKS=\\\"base udev autodetect modconf block keymap encrypt lvm2 filesystems keyboard shutdown fsck usr\\\""
+grep -q "$search" "$file" && sed -i "s#$search#$replace#" "$file" || echo "$replace" >> "$file"
+mkinitcpio -p linux
+
+echo "installing bootctl..."
+bootctl --path=/boot install
+> /boot/loader/loader.conf
+echo "default arch" >> /boot/loader/loader.conf
+echo "timeout 0" >> /boot/loader/loader.conf
+echo "editor 0" >> /boot/loader/loader.conf
+datUUID=blkid | sed -n '/nvme1n1/s/.*UUID=\"\([^\"]*\)\".*/\1/p'
+touch /boot/loader/entries/arch.conf
+echo "title Arch Linux" >> /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options cryptdevice=UUID=$datUUID:volume:allow-discards root=/dev/mapper/volume-root quiet rw" >> /boot/loader/entries/arch.conf
+echo "volume UUID=$datUUID none luks,discard" > /etc/crypttab
+
+pacman --noconfirm --needed -S dialog
+larbs() { curl -LO http://larbs.xyz/larbs.sh && bash larbs.sh ;}
+dialog --title "Install Luke's Rice" --yesno "This install script will easily let you access Luke's Auto-Rice Boostrapping Scripts (LARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && larbs
