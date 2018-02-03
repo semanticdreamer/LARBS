c36dc9d5767c35bfc10665e4bafe26c3d3ea7060
Matthias Geisler <matthias@openwebcraft.com>
2018-02-04 00:52:53 +0100

    bugfix: reverting (dump) nvme naming

--- src/arch-nvme1n1-efi-lvm-luks.sh
+++ src/arch-nvme1n1-efi-lvm-luks.sh
@@ -26,10 +26,10 @@ timedatectl set-ntp true
 echo "partitioning drive..."
 sleep 2
 sgdisk -og /dev/nvme1n1
-sgdisk -n 1:2048:+512MiB -t 1:ef00 /dev/nvme1n1p1
+sgdisk -n 1:2048:+512MiB -t 1:ef00 /dev/nvme1n1
 start_of='sgdisk -f /dev/nvme1n1'
 end_of='sgdisk -E /dev/nvme1n1'
-sgdisk -n 2:$start_of:$end_of -t 2:8e00 /dev/nvme1n1p2
+sgdisk -n 2:$start_of:$end_of -t 2:8e00 /dev/nvme1n1
 sgdisk -p /dev/nvme1n1
 
 echo "cryptsetup (luks) and lvm..."
