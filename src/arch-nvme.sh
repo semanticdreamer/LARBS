8e86e7ff567ba19cc572691a62cc232026d96df2
Matthias Geisler <matthias@openwebcraft.com>
2018-02-04 22:52:10 +0100

    refactored scripts, DRY for install device (still extra arch-nvme though)

--- src/arch-nvme.sh
+++ src/arch-nvme.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+INSTALL_DEVICE="/dev/nvme0n1"
+echo "${INSTALL_DEVICE}" > idevice
+
 #This is a lazy script I have for auto-installing Arch.
 #It's not officially part of LARBS, but I use it for testing.
-#DO NOT RUN THIS YOURSELF because Step 1 is it reformatting /dev/nvme1n1 WITHOUT confirmation,
+#DO NOT RUN THIS YOURSELF because Step 1 is it reformatting ${INSTALL_DEVICE} WITHOUT confirmation,
 #which means RIP in peace qq your data unless you've already backed up all of your drive.
 
 pacman -S --noconfirm dialog || (echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?" && exit)
@@ -10,9 +13,9 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane who doesn't mind deleting your entire /dev/nvme1n1 drive.\n\nThis script is only really for me so I can autoinstall Arch.\n\n Matthias"  15 60 || exit
+dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane who doesn't mind deleting your entire ${INSTALL_DEVICE} drive.\n\nThis script is only really for me so I can autoinstall Arch.\n\n Matthias"  15 60 || exit
 
-dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "Do you think I'm meming? Only select yes to DELET your entire /dev/nvme1n1 and reinstall Arch.\n\nTo stop this script, press no."  10 60 || exit
+dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "Do you think I'm meming? Only select yes to DELETE your entire ${INSTALL_DEVICE} and reinstall Arch.\n\nTo stop this script, press no."  10 60 || exit
 
 dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
 
@@ -27,7 +30,7 @@ fi
 
 timedatectl set-ntp true
 
-cat <<EOF | fdisk /dev/nvme1n1
+cat <<EOF | fdisk ${INSTALL_DEVICE}
 o
 n
 p
@@ -52,16 +55,16 @@ w
 EOF
 partprobe
 
-mkfs.ext4 /dev/nvme1n1p4
-mkfs.ext4 /dev/nvme1n1p3
-mkfs.ext4 /dev/nvme1n1p1
-mkswap /dev/nvme1n1p2
-swapon /dev/nvme1n1p2
-mount /dev/nvme1n1p3 /mnt
+mkfs.ext4 ${INSTALL_DEVICE}4
+mkfs.ext4 ${INSTALL_DEVICE}3
+mkfs.ext4 ${INSTALL_DEVICE}1
+mkswap ${INSTALL_DEVICE}2
+swapon ${INSTALL_DEVICE}2
+mount ${INSTALL_DEVICE}3 /mnt
 mkdir /mnt/boot
-mount /dev/nvme1n1p1 /mnt/boot
+mount ${INSTALL_DEVICE}1 /mnt/boot
 mkdir /mnt/home
-mount /dev/nvme1n1p4 /mnt/home
+mount ${INSTALL_DEVICE}4 /mnt/home
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
@@ -71,7 +74,7 @@ pacstrap /mnt base base-devel
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-curl https://raw.githubusercontent.com/semanticdreamer/LARBS/master/src/chroot-nvme1n1.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+curl https://raw.githubusercontent.com/semanticdreamer/LARBS/master/src/chroot-nvme.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
 
 cat comp > /mnt/etc/hostname && rm comp
 
