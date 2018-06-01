ecc5f308bf1719318b16ce78c1998925f5d0b291
Matthias Geisler <matthias@openwebcraft.com>
2018-06-01 11:36:46 +0200

    jq added

--- src/ricing.sh
+++ src/ricing.sh
@@ -88,6 +88,7 @@ yaourt -S --noconfirm --needed postman-bin
 sudo pacman -S --noconfirm --needed sqlite sqlitebrowser
 yaourt -S --noconfirm --needed git-town
 yaourt -S --noconfirm --needed bat
+sudo pacman -S --noconfirm --needed jq
 
 # SQLite's REGEXP calls a user defined function
 # http://www.sqlite.org/lang_expr.html
