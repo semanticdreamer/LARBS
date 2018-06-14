bed2876f7e6977cd937e631f91b6c7a6688aeb07
Matthias Geisler <matthias@openwebcraft.com>
2018-06-14 12:46:03 +0200

    removed symlink for etcher-electron

--- src/ricing.sh
+++ src/ricing.sh
@@ -159,7 +159,6 @@ yaourt -S --noconfirm --needed fritzing
 
 # tools
 yaourt -S --noconfirm --needed etcher
-sudo ln -s /opt/Etcher/etcher-electron /usr/bin/etcher-electron
 yaourt -S --noconfirm --needed etcher-cli-bin
 yaourt -S --noconfirm --needed fman
 yaourt -S --noconfirm --needed geteltorito
