2a620e5a46c0a50bb7a39a699dd79d05a3dea59b
Ian Mubangizi <io@ianmubangizi.com>
2018-08-04 07:02:20 +0000

    Forked Luke's LARBS git repo

--- larbs.sh
+++ larbs.sh
@@ -22,8 +22,8 @@ while getopts ":a:r:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/ianmubangizi/voidrice.git"
+[ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/ianmubangizi/LARBS/master/progs.csv"
 [ -z ${aurhelper+x} ] && aurhelper="packer"
 
 ###
